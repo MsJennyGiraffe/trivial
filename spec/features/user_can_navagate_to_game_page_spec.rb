45120588e1235f7ebe1eea0687418266a01233df
Jennifer <jennifersoden@hotmail.com>
2016-07-28 21:22:53 -0600

    clean up feature test for round index

--- spec/features/user_can_navagate_to_game_page_spec.rb
+++ spec/features/user_can_navagate_to_game_page_spec.rb
@@ -3,15 +3,19 @@ require "rails_helper"
 RSpec.feature "user can navagate to game page" do
   scenario "a user can navagate to the game page" do
     VCR.use_cassette("gamepage") do
+      question_one = "How fast did the Millennium Falcon make the Kessel Run?"
+      question_two = "What, according to Luke Skywalker was the Emperor's weakness?"
+      answer_one = "The Millennium Falcon made the Kessel Run in in less than twelve parsecs."
+      answer_two = "According to Luke Skywalker, the Emperor's overconfidence was his weakness."
       visit "/"
       click_link "Let's Play"
 
       expect(current_path).to eq("/rounds")
 
-      expect(page).to have_content("How fast did the Millennium Falcon make the Kessel Run?")
-      expect(page).to have_content("The Millennium Falcon made the Kessel Run in in less than twelve parsecs.")
-      expect(page).to have_content("What, according to Luke Skywalker was the Emperor's weakness?")
-      expect(page).to have_content("According to Luke Skywalker, the Emperor's overconfidence was his weakness.")
+      expect(page).to have_content(question_one)
+      expect(page).to have_content(answer_one)
+      expect(page).to have_content(question_two)
+      expect(page).to have_content(answer_two)
     end
   end
   end
